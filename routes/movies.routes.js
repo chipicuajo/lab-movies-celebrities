5e90bd61f3df52361ba5990f76fd40a296983750
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-19 19:38:15 +0200

    IT6 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
 
+let Celebrity = require('../models/Celebrity.model')
+let Movie = require('../models/Movie.model')
+
+router.get("/movies/create", (req,res)=>{
+    Celebrity.find()
+    .then((data) => res.render('movies/new-movie.hbs', {celebrities: data}))
+    .catch((err) => console.log(err.message));
+})
+
+router.post("/movies/create", (req,res)=>{
+    const {title, genre, plot, cast} = req.body
+    Movie.create({title, genre, plot, cast})
+    .then(() => res.redirect('/movies'))
+    .catch((err) => console.log(err.message))
+})
+
 module.exports = router;
