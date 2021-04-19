42c32ea9d94f087a9ea077f55d55e5d4d0722cd9
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-19 19:59:04 +0200

    IT7 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 let Celebrity = require('../models/Celebrity.model')
 let Movie = require('../models/Movie.model')
 
+/* CREATE */
 router.get("/movies/create", (req,res)=>{
     Celebrity.find()
     .then((data) => res.render('movies/new-movie.hbs', {celebrities: data}))
@@ -16,4 +17,23 @@ router.post("/movies/create", (req,res)=>{
     .catch((err) => console.log(err.message))
 })
 
+/* INDEX ALL MOVIES*/
+router.get("/movies", (req, res) => {
+    Movie.find()
+    .then((data) => res.render("movies/movies.hbs", {movies: data}))
+    .catch((err) => console.log(err.message))
+})
+
+/*SHOW MOVIE DETAILS */
+router.get('/movies/:id', (req,res)=>{
+    const {id} = req.params;
+    Movie.findById(id)
+    .populate('cast')
+    .then((data) => {
+        res.render('movies/movie-details.hbs', {movie: data} )
+    }).catch((err) => {
+        console.log(err.message)
+    });
+
+})
 module.exports = router;
