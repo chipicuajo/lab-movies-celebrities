0363fee88c630910a787c532ff2f841cd9fba9e1
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-19 17:38:04 +0200

    IT4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 let Celebrity = require('../models/Celebrity.model')
 
+
+//CREATE New Celebs
 router.get('/celebrities/create', (req,res,next)=>{
   res.render('celebrities/new-celebrity.hbs')
 })
@@ -14,6 +16,14 @@ router.post('/celebrities/create', (req, res) => {
         res.render("celebrities/new-celebrity.hbs")
         console.log(err.message)
     });
+})
+
+
+//FIND All Celebs
+router.get('/celebrities', (req, res)=>{
+    Celebrity.find()
+    .then((result) => res.render('celebrities/celebrities.hbs', {celebrities: result}))
+    .catch((err) => console.log('ERROR: ', err));
 })
 
 module.exports = router;
